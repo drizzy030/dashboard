dea92b6abef0ae79d8d0d993ef287d69052396bb
drizzy030 <maximilianschneiderbln@gmail.com>
2024-03-02 14:13:51 +0100

    d

---
+++ src/components/productsdev/productTable.tsx
@@ -0,0 +1,59 @@
+"use client";
+import {
+  Input,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+import { Product } from "@prisma/client";
+import { useState } from "react";
+
+export function ProductTable({ products }: { products: Product[] }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  return (
+    <div className="space-y-3">
+      <Input
+        isClearable
+        placeholder="Search by name..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>ID</TableColumn>
+          <TableColumn>SHOP</TableColumn>
+          <TableColumn>NAME</TableColumn>
+          <TableColumn>DESCRIPTION</TableColumn>
+          <TableColumn>PRICE</TableColumn>
+          <TableColumn>QUANTITY</TableColumn>
+          <TableColumn>TYPE</TableColumn>
+          <TableColumn>SERIALS</TableColumn>
+          <TableColumn>CREATED AT</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent={"No products."}>
+          {filteredProducts.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.id}</TableCell>
+              <TableCell>{product.shop}</TableCell>
+              <TableCell>{product.title}</TableCell>
+              <TableCell>{product.description}</TableCell>
+              <TableCell>{product.price}</TableCell>
+              <TableCell>{product.quantity}</TableCell>
+              <TableCell>{product.type}</TableCell>
+              <TableCell>{product.serial}</TableCell>
+              <TableCell>{product.createdAt.toLocaleString()}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- src/components/productsdev/products.tsx
+++ src/components/productsdev/products.tsx
@@ -1,13 +1,16 @@
-// import { Test } from "~/components/products/table";
+import { ProductTable } from "~/components/productsdev/productTable";
+import { auth } from "~/server/auth";
+
 export async function Products() {
+  const session = await auth();
   return (
     <section>
       <div className="h-full lg:px-6">
-        <div className="mx-auto flex w-full max-w-[90rem] flex-wrap justify-center gap-4  px-4 pt-3 sm:pt-10 lg:px-0 xl:flex-nowrap xl:gap-6">
+        <div className="mx-auto flex w-full max-w-[90rem] flex-wrap justify-center gap-4  px-4 py-3 sm:pt-10 lg:px-0 xl:flex-nowrap xl:gap-6">
           <div className="mt-6 flex w-full flex-col gap-6">
             <div className="flex flex-col gap-2">
               <h3 className="text-xl font-semibold">Products</h3>
-              {/* <Test products={products} /> */}
+              <ProductTable products={session?.user.products!} />
             </div>
           </div>
         </div>
