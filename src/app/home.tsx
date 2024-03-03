9f9facb9f17a789657cc10f204816b76d178faca
drizzy030 <maximilianschneiderbln@gmail.com>
2024-03-03 21:35:44 +0100

    f

---
+++ src/app/home.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import { TypeAnimation } from "react-type-animation";
+
+export function HomePage() {
+  const [textColor, setTextColor] = useState("");
+
+  return (
+    <div
+      style={{
+        color: textColor,
+      }}
+    >
+      <TypeAnimation
+        repeat={Infinity}
+        className="lg:text:3xl text-3xl  font-bold xl:text-5xl"
+        sequence={[
+          "Dashboard Based On ",
+          () => [],
+          "Sellix",
+          () => setTextColor("#6A3CE2"),
+        ]}
+      />
+    </div>
+  );
+}
