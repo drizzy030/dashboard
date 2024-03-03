094529c59d3dc5de1b325cd210b8f39d4d25c118
drizzy030 <maximilianschneiderbln@gmail.com>
2024-03-03 14:18:54 +0100

    f

--- next.config.js
+++ next.config.js
@@ -3,13 +3,8 @@
  * for Docker builds.
  */
 await import("./src/env.js");
-const { webpackPlugin: utwm } = require("unplugin-tailwindcss-mangle");
+
 /** @type {import("next").NextConfig} */
-const config = {
-  webpack: (config) => {
-    config.plugins.push(utwm());
-    return config;
-  },
-};
+const config = {};
 
 export default config;
